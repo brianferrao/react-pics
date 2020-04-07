c45e507e058c2c0965fd0b4faeaed006dc8dd75b
brianferrao <shirva.brian@gmail.com>
2020-04-06 23:55:12 -0400

    Initial commit with basic App scaffolding

---
+++ src/components/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import SearchInput from './SearchInput';
+
+const App = () => {
+    return (
+        <div className="ui container">
+            <SearchInput />
+        </div>
+    );
+}
+
+export default App;
---
+++ src/components/SearchInput.js
@@ -0,0 +1,30 @@
+import React from 'react';
+
+class SearchInput extends React.Component {
+
+    state = { searchText: ''};
+
+    onInputChange = (event) => {
+        this.setState({searchText: event.target.value});
+    }
+
+    onFormSubmit = (event) => {
+        event.preventDefault();
+        console.log(this.state.searchText);
+    }
+
+    render() {
+        return (
+            <div className="ui segment">
+                <form className="ui form" onSubmit={this.onFormSubmit}>
+                    <div className="field"> 
+                        <label>Search Text:</label>
+                        <input type="text" value={this.state.searchText} onChange={this.onInputChange}/>
+                    </div>                    
+                </form>
+            </div>
+        );
+    }
+}
+
+export default SearchInput;
